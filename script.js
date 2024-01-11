fcced3e608a346b2c87c82bd194ab7ce80d5cd0c
Luffy <148352199+ClumsyCoder04@users.noreply.github.com>
2024-01-11 23:07:52 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,137 +1,35 @@
-/* let day = parseInt(prompt("input a number between 1 and 7"));
-if (day === 5) {
-    alert("Today is Thursday");
-} else if (!day) {
-    day = parseInt(prompt("Enter a real number"));
-} else if (day === 1) {
-    alert("Today is Sunday");
-} else if (day === 2) {
-    alert("Today is Monday");
-} else if (day === 3) {
-    alert("Today is Tuesday");
-} else if (day === 4) {
-    alert("Today is Wednesday");
-} else if (day === 6) {
-    alert("Today is Friday");
-} else if (day === 7) {
-    alert("Today is Friday");
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
+
+
+const mixer = mixitup('.p-container', {
+    selectors: {
+        target: '.p-content'
+    },
+    animation: {
+        duration: 400
+    }
+});
+
+const linkPortfolio = document.querySelectorAll('.p-item')
+
+function activePortfolio(){
+    if(linkPortfolio){
+        linkPortfolio.forEach(l => l.classList.remove('active-portfolio'))
+        this.classList.add('active-portfolio')
+    }
 }
 
-console.log('These are the work day of the week')
-const day = parseInt(prompt('input a number between 1 and 5'))
-switch (day) {
-    case 1:
-        console.log("Monday");
-        break;
-    case 2:
-        console.log("Tuesday");
-        break;
-    case 3:
-        console.log("Wednesday");
-        break;
-    case 4:
-        console.log("Thursday");
-        break;
-    case 5:
-        console.log("Friday");
-        break;
-    default:
-        console.log("That's not a number between 1 and 5");
-
-}
-
-else {
-    alert('Wrong Number');
-}
-for (let i = 0; i <= 10; i++) {
-    console.log(i);
-}
-const firstname = prompt("Enter Your First Name")
-if(!firstname){
-    const firstname = prompt("Enter Your First Name")
-} else{
-    console.log(`Your first name is ${firstname}`)
-} 
-let names = ['Ade','Yinka','Ghafar','Azeez', 'Ayomide','Fathia','Ganiu']
-                                //Results
-names
-(7) ['Ade', 'Yinka', 'Ghafar', 'Azeez', 'Ayomide', 'Fathia', 'Ganiu']
-names.pop()
-'Ganiu'
-names.push('Ganiu')
-7
-names
-(7) ['Ade', 'Yinka', 'Ghafar', 'Azeez', 'Ayomide', 'Fathia', 'Ganiu']
-names.shift()
-'Ade'
-names
-(6) ['Yinka', 'Ghafar', 'Azeez', 'Ayomide', 'Fathia', 'Ganiu']
-names.unshift('Ade')
-7
-names
-(7) ['Ade', 'Yinka', 'Ghafar', 'Azeez', 'Ayomide', 'Fathia', 'Ganiu'] 
-*/
-
-// let names = ['Ade','Yinka','Ghafar','Adebowale', 'Ayomide','Fathia','Ganiu']
-// let surnames = ['Adebayo']
-// let fullname = names.concat(surnames)
+linkPortfolio.forEach(l => l.addEventListener('click', activePortfolio))
 
 
-
-// for (let day = 0; day <= 7; day++) {
-//     // console.log(i);
-//     day = parseInt(prompt("Enter a real number"));
-// }
-// let day = parseInt(prompt("input a number between 1 and 7"));
-// while (!day) {
-//     let day = parseInt(prompt("Enter a real number"));
-//     if (day === 5) {
-//         alert("Today is Thursday");
-//     } else if (day === 1) {
-//         alert("Today is Sunday");
-//     } else if (day === 2) {
-//         alert("Today is Monday");
-//     } else if (day === 3) {
-//         alert("Today is Tuesday");
-//     } else if (day === 4) {
-//         alert("Today is Wednesday");
-//     } else if (day === 6) {
-//         alert("Today is Friday");
-//     } else if (day === 7) {
-//         alert("Today is Friday");
-//     }
-// }
-
-// let names = ['Ade','Yinka','Ghafar','Adebowale', 'Ayomide','Fathia','Ganiu'];
-// const age = [19, 18, 17, 20, 17, 12, 15]
-
-
-// for (let i = 0; i < names.length; i++) {
-//     console.log(`${names[i]} is ${age[i]} years old`);
-//   }
-
-//   for (const [index, name] of names.entries()) {
-//     console.log(`${name} is ${age[index]} years old`);
-//   }
-//   for (let name of names) {
-//     console.log(name);
-  
-//     for (let i = 0; i < names.length; i++) {
-//       if (names[i] === name) {
-//         console.log(`${name} is ${age[i]} years old`);
-//       }
-//     }
-//   }
-  
-
-
-//  for (let day = 0; day <= 7; day++) {
-//       console.log(day);
-// }
-
-function rant(message){
-    console.log(message.toUpperCase());
-    console.log(message.toUpperCase());
-    console.log(message.toUpperCase());
-}
-rant ('idiot');
